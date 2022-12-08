988e551ab7a6a603827ff5f54598c4a8dd595f4f
Leandro Fusetti <leandrofusetti@gmail.com>
2022-12-08 19:17:27 -0300

    react estados, terminado

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-
-const studentName = "";
+import Botones from "./Botones.jsx"
+const studentName = "Leandro";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  
+  return (<div>
+    <h1>hola herny</h1>
+    <h3>
+      {studentName}
+    </h3>
+    <ul>
+      {techSkills.map((tecnicas, index)=>
+        <li key={index}>{tecnicas}</li>
+      )}
+    </ul>
+    <Botones alerts={alerts}/>
+    
+  </div>)
 }
 
 // Esto lo exportamos para los tests
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -5,11 +5,71 @@ import Animals from '../Animals/Animals';
 import Species from '../Species/Species';
 import './Zoo.module.css';
 
+
 export default function Zoo() {
    /* Escribe acá tu código */
+   const [zoo, setZoo] = React.useState({
+   zooName: "",
+   animals: [],
+   species: [],
+   allAnimals: [],
+})
+  
+React.useEffect(()=>{
+   fetch('http://localhost:3001/zoo')
+   .then((res) => res.json())
+   .then((data) =>
+      setZoo({
+         ...zoo,
+         animals: data.animals,
+         species: data.species,
+         allAnimals: data.animals,
+      })
+   )
+   .catch((error) => console.log(error))
+},[])
+
+const handleSpecies = (e)=>{
+   const specie = e.target.value
+   console.log(specie)
+   const filtrado = zoo.Allanimals.filter(animal=> animal.specie === specie)
+   console.log(filtrado);
+   
+   setZoo({
+      ...zoo,
+      animals: filtrado
+   })
+   
+}
+
+const handleAllSpecies = (e)=>{
+   const specie = e.target.value
+   console.log(specie)
+   const filtrado = zoo.Allanimals.filter(animal=> animal.specie === specie)
+   console.log(filtrado);
+   
+   setZoo({
+      ...zoo,
+      animals: filtrado
+   })
+}
+const handleInputChange = (e)=>{
+   const valor = e.target.value
+   
+   setZoo({
+      ...zoo,
+      zooName: valor
+   })
+}
+
+
    return (
       <div>
-         <h1>-</h1>
+         <label htmlFor="zoo">Zoo Name:</label>
+         <input onChange={handleInputChange} type="text" value={ zoo.zooName} />
+         <h1>{zoo.zooName}</h1>
+         <Species species={zoo.species} handleSpecies={handleSpecies} handleAllSpecies={handleAllSpecies} />
+         <Animals animals={zoo.animals}/>
       </div>
    );
 }
