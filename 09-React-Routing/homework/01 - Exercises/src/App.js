d55b861f8689385d021abf1fd539b9d2d7de22f5
Leandro Fusetti <leandrofusetti@gmail.com>
2022-12-18 03:40:08 -0300

    routing

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -6,7 +6,37 @@ import Shipping from "./components/Shipping/Shipping.jsx";
 import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
 /* eslint-disable */
+import {Route,Routes} from "react-router-dom";
+
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar/>
+      <Routes>
+      {/*   <Route path="/" component={Home}/>
+        <Route path="/shipping">
+          <Shipping/>
+        </Route>
+        <Route path="/promotions" children={()=> <Promotions/>}/>
+        <Route path="cruises/:id" render={()=><CardDetail/>}/> */}
+        {/* <Route path="/shipping" element={<Shipping/>}/> */}
+
+
+
+        <Route path="/" element={<Home/>}/>
+        <Route path="/shipping" element={<Shipping/>}/>
+          
+        <Route path="/promotions" element={<Promotions/>}/>
+        <Route path="/cruises/:id" element={<CardDetail/>}/> 
+
+
+
+
+      </Routes>
+
+
+
+    </div>
+    );
 }
